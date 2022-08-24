7104211c4a7e328946532e271e83dda9ebb451df
Timadey <dlktimothy@gmail.com>
2022-08-24 17:39:59 +0100

    [Add] new regex expression

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+print ARGV[0].scan(/(?<=from:)(\+?\d{1,11}|\w+(?=\]))/).join,
+",",
+ARGV[0].scan(/(?<=to:)(\+?\d{1,11}|\w+(?=\]))/).join,
+",",
+ARGV[0].scan(/(?<=flags:)(.+?)(?=\])/).join
