df0f936de5dfbde428fba8eb9ca1c301d3e160e8
Timadey <dlktimothy@gmail.com>
2022-08-24 18:01:14 +0100

    Update 100 textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-print ARGV[0].scan(/(?<=from:)(\+?\d{1,11}|\w+(?=\]))/).join,
-",",
-ARGV[0].scan(/(?<=to:)(\+?\d{1,11}|\w+(?=\]))/).join,
-",",
-ARGV[0].scan(/(?<=flags:)(.+?)(?=\])/).join
-STDOUT.flush
+puts ARGV[0].scan(/(?<=from:|to:|flags:)(.+?)(?=\])/).join(",")
